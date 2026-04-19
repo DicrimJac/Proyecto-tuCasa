adde6771c7b10818478affa0b82139390e0582b4
DicrimJac <j.liss.zamora@gmail.com>
2026-04-18 22:45:26 -0400

    conecion con BBDD y documentos solicitados

---
+++ Producto/CodigoFuente/lib/bbdd.ts
@@ -0,0 +1,10 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const supabaseUrl = Deno.env.get("SUPABASE_URL");
+const supabaseKey = Deno.env.get("SUPABASE_ANON_KEY");
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Faltan variables de entorno");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
