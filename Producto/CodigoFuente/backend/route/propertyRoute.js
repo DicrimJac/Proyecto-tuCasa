3df2a007cbea5cd1ec69e81720e6acb059e5c4e8
DicrimJac <j.liss.zamora@gmail.com>
2026-05-31 03:13:26 -0400

    ...

--- Producto/CodigoFuente/backend/route/propertyRoute.js
+++ Producto/CodigoFuente/backend/route/propertyRoute.js
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { PropertyController } from "../controller/propertyController.js";
 import { requireAuth } from "../middleware/auth.js";
+import { handler as analyzeHandler } from "../controller/analizy.js";
 
 const propertyRoute = new Hono();
 const propertyController = new PropertyController();
@@ -23,5 +24,5 @@ propertyRoute.post("/", requireAuth, (c) => propertyController.createProperty(c)
 propertyRoute.put("/:id", requireAuth, (c) => propertyController.updateProperty(c));
 // DELETE /api/properties/:id
 propertyRoute.delete("/:id", requireAuth, (c) => propertyController.deleteProperty(c));
-
+propertyRoute.post("/analyze", analyzeHandler);
 export default propertyRoute;
